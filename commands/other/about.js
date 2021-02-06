811186b70a95370bc7e0929807991680b178481e
ZEROO202 <71737369+ZEROO202@users.noreply.github.com>
2021-02-06 17:57:16 +0300

    Update about.js

--- commands/other/about.js
+++ commands/other/about.js
@@ -13,7 +13,7 @@ module.exports = class WhoMadeMeCommand extends Command {
 
   run(message) {
     message.say(
-      'Made by @hyperzone#1185 with :heart: full code is available on GitHub https://github.com/galnir/Master-Bot'
+      'Made by <@689213459480707156>'
     );
   }
 };
