9c4e9af0b83be80f9ab817d8092a8c755273b7a5
MR.Z <71737369+ZEROO202@users.noreply.github.com>
2021-03-03 21:29:38 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,18 @@
+
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: "Display all commands and descriptions",
+  description: i18n.__("help.description"),
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle(`${message.client.user.username} Help`)
-      .setDescription("List of all commands")
-      .setColor("RANDOM");
-     
+      .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
+      .setDescription(i18n.__("help.embedDescription"))
+      .setColor("#F8AA2A");
+
     commands.forEach((cmd) => {
       helpEmbed.addField(
         `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
@@ -25,3 +26,5 @@ module.exports = {
     return message.channel.send(helpEmbed).catch(console.error);
   }
 };
+
+
